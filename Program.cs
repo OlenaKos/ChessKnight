deece8f8d9cc36fbbd0dc14b80f010b318cfa942
OlenaKos <48287055+OlenaKos@users.noreply.github.com>
2019-09-06 20:26:08 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,269 +3,127 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.InteropServices;
-using System.Drawing;
-using System.Threading;
+using System.Diagnostics;
 
-//using ConsoleExtender;
-
-//namespace ConsoleExtender
-//{
-//    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-//    public struct ConsoleFont
-//    {
-//        public uint Index;
-//        public short SizeX, SizeY;
-//    }
-
-//    public static class ConsoleHelper
-//    {
-//        //[DllImport("kernel32")]
-//        //public static extern bool SetConsoleIcon(IntPtr hIcon);
-
-//        //public static bool SetConsoleIcon(Icon icon)
-//        //{
-//        //    return SetConsoleIcon(icon.Handle);
-//        //}
-
-//        [DllImport("kernel32")]
-//        private extern static bool SetConsoleFont(IntPtr hOutput, uint index);
-
-//        private enum StdHandle
-//        {
-//            OutputHandle = -11
-//        }
-
-//        [DllImport("kernel32")]
-//        private static extern IntPtr GetStdHandle(StdHandle index);
-
-//        public static bool SetConsoleFont(uint index)
-//        {
-//            return SetConsoleFont(GetStdHandle(StdHandle.OutputHandle), index);
-//        }
-
-//        [DllImport("kernel32")]
-//        private static extern bool GetConsoleFontInfo(IntPtr hOutput, [MarshalAs(UnmanagedType.Bool)]bool bMaximize,
-//            uint count, [MarshalAs(UnmanagedType.LPArray), Out] ConsoleFont[] fonts);
-
-//        [DllImport("kernel32")]
-//        private static extern uint GetNumberOfConsoleFonts();
-
-//        public static uint ConsoleFontsCount
-//        {
-//            get
-//            {
-//                return GetNumberOfConsoleFonts();
-//            }
-//        }
-
-//        public static ConsoleFont[] ConsoleFonts
-//        {
-//            get
-//            {
-//                ConsoleFont[] fonts = new ConsoleFont[GetNumberOfConsoleFonts()];
-//                if (fonts.Length > 0)
-//                    GetConsoleFontInfo(GetStdHandle(StdHandle.OutputHandle), false, (uint)fonts.Length, fonts);
-//                return fonts;
-//            }
-//        }
-
-//    }
-//}
-
-namespace HorsePower
+namespace Knight3
 {
     class Program
     {
-        static int[][] arrDesk = new int[8][];
-        static int deskSize = 8;
-
-        static int[] row = new int[64];
-        static int[] col = new int[64];
-        static int[] ktmove1 = { -2, -1, 1, 2, 2, 1, -1, -2 };
-        static int[] ktmove2 = { 1, 2, 2, 1, -1, -2, -2, -1 };
-
-        static int i = 0, j = 0, g = 0, h = 0, move_num = 1;
+        static int deskSize = 6;
+        static int[] bx = new int[deskSize * deskSize];
+        static int[] by = new int[deskSize * deskSize];
+        static int[][] board = new int[deskSize][];
 
 
         static void Main(string[] args)
         {
-            for (int k = 0; k < 8; k++)
+
+            for (int k = 0; k < deskSize; k++)
             {
-                arrDesk[k] = new int[8];
+                board[k] = new int[deskSize];
             }
             Console.WriteLine("Please enter a first x position");
-            i = Int32.Parse(Console.ReadLine()) - 1;
-            g = i;
+            bx[0] = Int32.Parse(Console.ReadLine());
+
             Console.WriteLine("Please enter a first y position");
-            j = Int32.Parse(Console.ReadLine()) - 1;
-            h = j;
-            addKnight();
-        }
+            by[0] = Int32.Parse(Console.ReadLine());
 
-        private static void addKnight()
-        {
-            int b, e;
-
-            //mark cell as attended and remember coordinates of cell
-            arrDesk[i][j] = move_num;
-            row[move_num] = i;
-            col[move_num] = j;
-            move_num++;
-
-            //check 8 possible knight moves
-            for (int a = 0; a <= 7; a++)
+            int s = 1;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            if (steps(0))
             {
-                // if all possible moves are done print them
-                if (move_num >= 64)
+                for (int i = 0; i < deskSize * deskSize; ++i)
                 {
-                    writeBoardExt();
-                    //writeBoardReDraw();
-                    Console.ReadLine();
-                    System.Environment.Exit(1);
-                }
-
-                //check column and row for the next move
-                b = i + ktmove1[a];
-                e = j + ktmove2[a];
-
-                //checking that after making a move_num knight is still on the chess desk
-                if (b < 0 || b > 7 || e < 0 || e > 7)
-                    continue;
-
-                //checking if we already attended that cell
-                if (arrDesk[b][e] != 0)
-                    continue;
-                i = b;
-                j = e;
-                addKnight();
-
-            }
-            //decrease counter of moves and try to make next move
-            move_num--;
-
-            //clear cell previously knight possessed
-            arrDesk[row[move_num]][col[move_num]] = 0;
-            move_num--; //try to do next step
-
-            i = row[move_num];
-            j = col[move_num];
-            move_num++;
-
-
-        }
-
-        private static void writeBoardReDraw()
-        {
-            int[][] arrDeskTemp = new int[8][];
-            for (int k = 0; k < deskSize; k++)
-            {
-                arrDeskTemp[k] = new int[8];
-            }
-
-            InitializeTempArray(arrDeskTemp);
-
-            int step = 0;
-            for (int k = 0; k < deskSize; k++)
-            {
-                for (int n = 0; n < deskSize; n++)
-                {
-                    Thread.Sleep(1000);
-                    Console.BackgroundColor = ConsoleColor.Black;
-                    Console.Clear();
-                    addNewStepToArray(step, arrDeskTemp, arrDesk);
-                    step++;
-                    PrintTempArray(arrDeskTemp);
+                    board[bx[i] - 1][by[i] - 1] = s;
+                    ++s;
                 }
             }
-        }
+            else
+                Console.WriteLine("No decision");
 
-        private static void InitializeTempArray(int[][] arrDeskTemp)
-        {
-            for (int i = 0; i < deskSize; i++)
-            {
-                for (int j = 0; j < deskSize; j++)
-                {
-                    arrDeskTemp[i][j] = 0;
-                    if (i == g && j == h)
-                    {
-                        arrDeskTemp[i][j] = 1;
-                    }
-                }
-            }
-        }
-
-        private static void addNewStepToArray(int step, int[][] arrDeskTemp, int[][] arrDesk)
-        {
+            sw.Stop();
+            Console.WriteLine("Time spent on solution search(sec): {0}", sw.Elapsed);
 
             for (int i = 0; i < deskSize; i++)
             {
                 for (int j = 0; j < deskSize; j++)
                 {
-
-                    if (arrDesk[i][j] == step)
-                    {
-                        arrDeskTemp[i][j] = arrDesk[i][j];
-                        break;
-                    }
+                    Console.WriteLine("board[i][j] = {0}", board[i][j]);
                 }
             }
+            writeBoardExt();
 
+            Console.ReadLine();
 
         }
 
-        private static void PrintTempArray(int[][] arrDeskTemp)
+        private static bool steps(int c)
         {
+            if (c + 1 >= deskSize * deskSize)
+                return true;
 
-            string s;
-            for (int i = 0; i < 8; i++)
+            for (int dx = -2; dx <= 2; ++dx)
             {
-                for (int j = 0; j < 8; j++)
+                for (int dy = -2; dy <= 2; ++dy)
                 {
+                    if (Math.Abs(dx) + Math.Abs(dy) != 3)
+                        continue;
 
-                    if (i % 2 == 0)
-                    {
-                        if (j % 2 == 0)
-                        {
-                            Console.BackgroundColor = ConsoleColor.White;
+                    int xn = bx[c] + dx, yn = by[c] + dy;
+                    if (xn < 1 || xn > deskSize || yn < 1 || yn > deskSize)
+                        continue;
 
-                            s = String.Format("{0,2}", arrDeskTemp[i][j]) + " ";
-                            Console.Write("{0}", s);
-                        }
-                        else
-                        {
-                            Console.BackgroundColor = ConsoleColor.DarkRed;
-                            s = String.Format("{0,2}", arrDeskTemp[i][j]) + " ";
-                            Console.Write("{0}", s);
-                        }
-                    }
-                    else
-                    {
-                        if (j % 2 != 0)
-                        {
-                            Console.BackgroundColor = ConsoleColor.White;
-                            s = String.Format("{0,2}", arrDeskTemp[i][j]) + " ";
-                            Console.Write("{0}", s);
+                    if (сhecking(xn, yn, c))
+                        continue;
 
-                        }
-                        else
-                        {
-                            Console.BackgroundColor = ConsoleColor.DarkRed;
-                            s = String.Format("{0,2}", arrDeskTemp[i][j]) + " ";
-                            Console.Write("{0}", s);
-                        }
-                    }
+                    bx[c + 1] = xn; by[c + 1] = yn;
+                    if (steps(c + 1))
+                        return true;
                 }
-                Console.WriteLine();
             }
+            return false;
         }
+
+        private static bool сhecking(int x, int y, int c)
+        {
+            for (int i = 0; i <= c; ++i)
+                if (bx[i] == x && by[i] == y)
+                    return true;
+            return false;
+        }
+
+        //private static void writeBoardReDraw()
+        //{
+        //    int[][] arrDeskTemp = new int[8][];
+        //    for (int k = 0; k < deskSize; k++)
+        //    {
+        //        arrDeskTemp[k] = new int[deskSize];
+        //    }
+
+        //    InitializeTempArray(arrDeskTemp);
+
+        //    int step = 0;
+        //    for (int k = 0; k < deskSize; k++)
+        //    {
+        //        for (int n = 0; n < deskSize; n++)
+        //        {
+        //            Thread.Sleep(1000);
+        //            Console.BackgroundColor = ConsoleColor.Black;
+        //            Console.Clear();
+        //            addNewStepToArray(step, arrDeskTemp, arrDesk);
+        //            step++;
+        //            PrintTempArray(arrDeskTemp);
+        //        }
+        //    }
+        //}
 
         private static void writeBoardExt()
         {
             string s;
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < deskSize; i++)
             {
-                for (int j = 0; j < 8; j++)
+                for (int j = 0; j < deskSize; j++)
                 {
                     if (i % 2 == 0)
                     {
@@ -273,15 +131,14 @@ namespace HorsePower
                         {
                             Console.BackgroundColor = ConsoleColor.White;
 
-                            s = String.Format("{0,2}", arrDesk[i][j]) + " ";
+                            s = String.Format("{0,2}", board[i][j]) + " ";
                             Console.Write("{0}", s);
                         }
                         else
                         {
                             Console.BackgroundColor = ConsoleColor.DarkRed;
-                            s = String.Format("{0,2}", arrDesk[i][j]) + " ";
+                            s = String.Format("{0,2}", board[i][j]) + " ";
                             Console.Write("{0}", s);
-                            //Console.Write("{0}\t", arrDesk[i][j]);
                         }
                     }
                     else
@@ -289,14 +146,14 @@ namespace HorsePower
                         if (j % 2 != 0)
                         {
                             Console.BackgroundColor = ConsoleColor.White;
-                            s = String.Format("{0,2}", arrDesk[i][j]) + " ";
+                            s = String.Format("{0,2}", board[i][j]) + " ";
                             Console.Write("{0}", s);
 
                         }
                         else
                         {
                             Console.BackgroundColor = ConsoleColor.DarkRed;
-                            s = String.Format("{0,2}", arrDesk[i][j]) + " ";
+                            s = String.Format("{0,2}", board[i][j]) + " ";
                             Console.Write("{0}", s);
                         }
                     }
@@ -308,3 +165,69 @@ namespace HorsePower
         }
     }
 }
+
+
+//board[i][j] = 1
+//board[i][j] = 12
+//board[i][j] = 9
+//board[i][j] = 6
+//board[i][j] = 3
+//board[i][j] = 14
+//board[i][j] = 17
+//board[i][j] = 20
+//board[i][j] = 10
+//board[i][j] = 7
+//board[i][j] = 2
+//board[i][j] = 13
+//board[i][j] = 18
+//board[i][j] = 21
+//board[i][j] = 4
+//board[i][j] = 15
+//board[i][j] = 31
+//board[i][j] = 28
+//board[i][j] = 11
+//board[i][j] = 8
+//board[i][j] = 5
+//board[i][j] = 16
+//board[i][j] = 19
+//board[i][j] = 22
+//board[i][j] = 64
+//board[i][j] = 25
+//board[i][j] = 32
+//board[i][j] = 29
+//board[i][j] = 36
+//board[i][j] = 23
+//board[i][j] = 48
+//board[i][j] = 45
+//board[i][j] = 33
+//board[i][j] = 30
+//board[i][j] = 27
+//board[i][j] = 24
+//board[i][j] = 49
+//board[i][j] = 46
+//board[i][j] = 37
+//board[i][j] = 58
+//board[i][j] = 26
+//board[i][j] = 63
+//board[i][j] = 52
+//board[i][j] = 35
+//board[i][j] = 40
+//board[i][j] = 57
+//board[i][j] = 44
+//board[i][j] = 47
+//board[i][j] = 53
+//board[i][j] = 34
+//board[i][j] = 61
+//board[i][j] = 50
+//board[i][j] = 55
+//board[i][j] = 42
+//board[i][j] = 59
+//board[i][j] = 38
+//board[i][j] = 62
+//board[i][j] = 51
+//board[i][j] = 54
+//board[i][j] = 41
+//board[i][j] = 60
+//board[i][j] = 39
+//board[i][j] = 56
+//board[i][j] = 43
